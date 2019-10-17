807b2029eac268ce3601d343c12fff22ebaaf2a6
tangs <tangsong1988kaka@gmail.com>
2019-10-17 18:40:22 +0800

    update.

--- lib/content.dart
+++ lib/content.dart
@@ -1,130 +1,141 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:lpinyin/lpinyin.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart' show rootBundle;
+// import 'package:lpinyin/lpinyin.dart';
 
-class ContentList extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _ContentListState();
-}
+// class ContentList extends StatefulWidget {
+//   ContentList({Key key}) : super(key: key);
 
-class Cell {
-  int type;
-  String title;
-  List<String> answers;
-  String pinyin;
-}
+//   final ValueChanged<void> onChanged;
 
-class _ContentListState extends State<ContentList> {
-  List<Cell> datas;
-  int loadCnt;
-  String wordkey;
-  int type;
+//   // setData(int type, String keyword) {
+//   //   this.type = type;
+//   //   this.keyword = keyword;
+//   //   if (state != null) {
+//   //     state.setData(type, keyword);
+//   //   }
+//   // }
 
-  _ContentListState(): super() {
-    datas = List();
-    loadCnt = 0;
-    wordkey = '';
-    type = 3;
-    loadConfigs();
-  }
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _ContentListState();
+//   }
+// }
 
-  setWordkey(key) {
-    this.wordkey = key;
-    this.setState((){});
-  }
+// class Cell {
+//   int type;
+//   String title;
+//   List<String> answers;
+//   String pinyin;
+// }
 
-  setType(type) {
-    this.type = type;
-    this.setState((){});
-  }
+// class _ContentListState extends State<ContentList> {
+//   List<Cell> datas;
+//   int loadCnt;
+//   String keyword = '';
+//   int type = -1;
 
-  getData() {
-    if (this.wordkey == null || this.wordkey.length == 0) {
-      return datas;
-    }
-    var key = this.wordkey.toLowerCase();
-    List<Cell> curDatas;
-    for (var data in datas) {
-      if (data.pinyin.indexOf(key) != -1) {
-        curDatas.add((data));
-      }
-    }
-    return curDatas;
-  }
+//   _ContentListState(): super() {
+//     // print(type);
+//     datas = List();
+//     loadCnt = 0;
+//     loadConfigs();
+//   }
 
-  loadConfig(String path, int type) {
-    rootBundle.loadString(path).then((String txt) {
-      // print(txt);
-      for (var line in txt.split('\n')) {
-        var words = line.split(',');
-        if (words.length < 2) continue;
-        var cell = Cell();
-        cell.type = type;
-        cell.title = words[1];
-        cell.answers = words.sublist(2);
-        cell.pinyin = PinyinHelper.getShortPinyin(cell.title).toLowerCase();
-        datas.add(cell);
-      }
-      if (++loadCnt == 3) {
-        this.setState(() {
+//   setData(int type, String keyword) {
+//     this.type = type;
+//     this.keyword = keyword;
+//     print('k:' + keyword);
+//     this.setState((){});
+//   }
 
-        });
-      }
-    });
-  }
+//   getData() {
+//     print(this.keyword);
+//     if (this.keyword == null || this.keyword.length == 0) {
+//       return datas;
+//     }
+//     var key = this.keyword.toLowerCase();
+//     List<Cell> curDatas;
+//     for (var data in datas) {
+//       if (data.pinyin.indexOf(key) != -1) {
+//         curDatas.add((data));
+//       }
+//     }
+//     return curDatas;
+//   }
 
-  loadConfigs() {
-    loadConfig('assets/RADIO.csv', 0);
-    loadConfig('assets/MULTISELECT.csv', 1);
-    loadConfig('assets/JUDGE.csv', 2);
-  }
+//   loadConfig(String path, int type) {
+//     rootBundle.loadString(path).then((String txt) {
+//       // print(txt);
+//       for (var line in txt.split('\n')) {
+//         var words = line.split(',');
+//         if (words.length < 2) continue;
+//         var cell = Cell();
+//         cell.type = type;
+//         cell.title = words[1];
+//         cell.answers = words.sublist(2);
+//         cell.pinyin = PinyinHelper.getShortPinyin(cell.title).toLowerCase();
+//         datas.add(cell);
+//       }
+//       if (++loadCnt == 3) {
+//         this.setState(() {
 
-  @override
-  Widget build(BuildContext context) {
-    var datas = this.getData();
-    var cnt = datas == null ? 0 : datas.length;
-    return ListView.builder(
-      padding: EdgeInsets.all(20),
-      itemCount: cnt,
-      itemBuilder: (BuildContext context, int idx) {
-        // return Text(datas[idx].title);
-        var data = datas[idx];
-        var children = List<Widget>();
-        children.add(
-          Row(children: <Widget>[
-            Expanded(
-              child: Text(
-                '题目:' + data.title,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xff000000)
-                ),
-              ),
-            ),],
-          )
-        );
-        for (var answer in data.answers) {
-          children.add(
-            Row(children: <Widget>[
-              Expanded(
-                child: Text(
-                  '答案:' + answer,
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Color(0xffff0000)
-                  ),
-                ),
-              ),],
-            )
-          );
-        }
-        children.add(Padding(padding: EdgeInsets.all(20),));
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: children,
-        );
-      },
-    );
-  }
+//         });
+//       }
+//     });
+//   }
 
-}
+//   loadConfigs() {
+//     loadConfig('assets/RADIO.csv', 0);
+//     loadConfig('assets/MULTISELECT.csv', 1);
+//     loadConfig('assets/JUDGE.csv', 2);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var datas = this.getData();
+//     var cnt = datas == null ? 0 : datas.length;
+//     return ListView.builder(
+//       padding: EdgeInsets.all(20),
+//       itemCount: cnt,
+//       itemBuilder: (BuildContext context, int idx) {
+//         // return Text(datas[idx].title);
+//         var data = datas[idx];
+//         var children = List<Widget>();
+//         children.add(
+//           Row(children: <Widget>[
+//             Expanded(
+//               child: Text(
+//                 '题目:' + data.title,
+//                 style: TextStyle(
+//                   fontSize: 20,
+//                   color: Color(0xff000000)
+//                 ),
+//               ),
+//             ),],
+//           )
+//         );
+//         for (var answer in data.answers) {
+//           children.add(
+//             Row(children: <Widget>[
+//               Expanded(
+//                 child: Text(
+//                   '答案:' + answer,
+//                   style: TextStyle(
+//                     fontSize: 18,
+//                     color: Color(0xffff0000)
+//                   ),
+//                 ),
+//               ),],
+//             )
+//           );
+//         }
+//         children.add(Padding(padding: EdgeInsets.all(20),));
+//         return Column(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: children,
+//         );
+//       },
+//     );
+//   }
+
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'content.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:lpinyin/lpinyin.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +18,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Cell {
+  int type;
+  String title;
+  List<String> answers;
+  String pinyin;
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -26,24 +34,125 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> 
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
 
-  void _incrementCounter() {
+  String keyword = '';
+  int type = 3;
+  List<Cell> datas;
+  int loadCnt = 0;
+
+  _MyHomePageState(): super() {
+    datas = List();
+    loadConfigs();
+  }
+
+  void update() {
     setState(() {
     });
   }
 
+  loadConfig(String path, int type) {
+    rootBundle.loadString(path).then((String txt) {
+      // print(txt);
+      for (var line in txt.split('\n')) {
+        var words = line.split(',');
+        if (words.length < 2) continue;
+        var cell = Cell();
+        cell.type = type;
+        cell.title = words[1];
+        cell.answers = words.sublist(2);
+        cell.pinyin = PinyinHelper.getShortPinyin(cell.title).toLowerCase();
+        datas.add(cell);
+      }
+      if (++loadCnt == 3) {
+        this.setState(() {
+
+        });
+      }
+    });
+  }
+
+  loadConfigs() {
+    loadConfig('assets/RADIO.csv', 0);
+    loadConfig('assets/MULTISELECT.csv', 1);
+    loadConfig('assets/JUDGE.csv', 2);
+  }
+
+  getData() {
+    print(this.keyword);
+    if (this.keyword == null || this.keyword.length == 0) {
+      return datas;
+    }
+    var key = this.keyword.toLowerCase();
+    List<Cell> curDatas = [];
+    for (var data in datas) {
+      if (data.pinyin.indexOf(key) != -1) {
+        curDatas.add((data));
+      }
+    }
+    return curDatas;
+  }
+
+  ListView getList() {
+    var datas = this.getData();
+    var cnt = datas == null ? 0 : datas.length;
+    return ListView.builder(
+      padding: EdgeInsets.all(20),
+      itemCount: cnt,
+      itemBuilder: (BuildContext context, int idx) {
+        // return Text(datas[idx].title);
+        var data = datas[idx];
+        var children = List<Widget>();
+        children.add(
+          Row(children: <Widget>[
+            Expanded(
+              child: Text(
+                '题目:' + data.title,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xff000000)
+                ),
+              ),
+            ),],
+          )
+        );
+        for (var answer in data.answers) {
+          children.add(
+            Row(children: <Widget>[
+              Expanded(
+                child: Text(
+                  '答案:' + answer,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xffff0000)
+                  ),
+                ),
+              ),],
+            )
+          );
+        }
+        children.add(Padding(padding: EdgeInsets.all(20),));
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: children,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    var content1 = ContentList();
     return Scaffold(
       appBar: AppBar(
         title: Row(
           children: <Widget>[            
             Expanded(
               child: TextFormField(
+                initialValue: this.keyword,
                 onChanged:(txt) {
-                  content1.
+                  print(txt);
+                  this.keyword = txt;
+                  this.update();
                 },
               ),
             ),
@@ -60,6 +169,11 @@ class _MyHomePageState extends State<MyHomePage>
                   child: TabBar(
                     unselectedLabelColor: Colors.green,
                     labelColor: Colors.orange,
+                    onTap: (idx) {
+                      print(idx);
+                      this.type = idx;
+                      this.update();
+                    },
                     tabs: <Widget>[
                       Tab(text: '单选题',),
                       Tab(text: '多选题',),
@@ -67,7 +181,7 @@ class _MyHomePageState extends State<MyHomePage>
                       Tab(text: '全部',),
                     ],
                     controller: TabController(
-                      initialIndex: 3,
+                      initialIndex: this.type,
                       length: 4,
                       vsync: this,
                     ),
@@ -76,16 +190,11 @@ class _MyHomePageState extends State<MyHomePage>
               ],
             ),
             Expanded(
-              child: content1,
+              child: getList(),
             )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
